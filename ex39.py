ed4a7e4ddefeb010ce551deb5278afe4b99a2609
Luis Felipe <felipeadriani2@gmail.com>
2021-12-02 14:48:20 -0300

    add

--- ex39.py
+++ ex39.py
@@ -1,7 +1,9 @@
 # Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
 
+from datetime import date
 ano = int(input("Digite o ano que nasceu: "))
-idade = 2021-ano
+ano_atual = date.today().year
+idade = ano_atual - ano
 
 if idade < 18:
     print(f"Voce tem {idade} anos, ainda não pode se alistar ao serviço militar.")
